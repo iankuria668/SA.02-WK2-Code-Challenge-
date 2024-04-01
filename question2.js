6bd1f45ffc28621485f5c1c31a8edce23c61f809
iankuria668 <iankuria668@gmail.com>
2024-04-01 12:28:08 +0300

    Corrections made

--- question2.js
+++ question2.js
@@ -1,14 +1,23 @@
 function arrayGenerator(num1,num2){
+     // Initialize an empty array to store the generated numbers
     const array = [];
-    if (num1 < num2){
+    // Check if num1 is less or equal to num2
+    if (num1 <= num2){
         for (let i=num1; i<=num2; i++){
+             // Push each number to the array
             array.push(i)
         }
+    // Check if num1 is less or equal to num2
     } else{
         for(let i=num1; i>=num2;i--){
+
+            // Push each number to the array
             array.push(i)
         }
     }
+    //Return the generated array
     return array;
 }
-console.log(arrayGenerator(1,7));
+// Example usage:
+console.log(arrayGenerator(4,7));
+console.log(arrayGenerator(-4,7));
