0a40501daa04d887a0607e0bf7ac9013fcf6dbaf
iankuria668 <iankuria668@gmail.com>
2024-04-01 12:06:09 +0300

    Question1 completed

---
+++ question1.js
@@ -0,0 +1,19 @@
+//Naming the function
+function swapCase(inputString) {
+    // Declaring the empty variables
+    let result = "";
+    // Separating the input into individual characters
+    for (let i = 0; i < inputString.length; i++) { 
+        const letter = inputString[i]; 
+        // Swapping the cases of the individual Characters
+        if (letter === letter.toUpperCase()) {
+            result += letter.toLowerCase();
+        } else {
+            result += letter.toUpperCase();
+        }
+    }
+    return result;
+}
+
+//Testing the function
+console.log(swapCase("The Quick Brown Fox"));
